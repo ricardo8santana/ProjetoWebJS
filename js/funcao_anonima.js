58ea7bec9217f89a5ed6ac41b18cd855690f4a35
ricardo8santana <luisriccardo@proton.me>
2026-03-24 16:54:39 -0300

    JavaScript Basic

---
+++ js/funcao_anonima.js
@@ -0,0 +1,8 @@
+//Criando função anonima 
+const somaValor = function(v1, v2){
+    return (v1 + v2)
+}
+//Executando a função anônima
+let resp = somaValor(10, 20)
+
+console.log(`A soma total é ${resp}`)
