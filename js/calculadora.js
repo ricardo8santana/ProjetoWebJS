85ed2e5b0e06c6dafbc84d3924e26afdf6af1c91
ricardo8santana <luisriccardo@proton.me>
2026-03-24 16:30:46 -0300

    JavaScript Basic

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,6 +1,14 @@
 //Criar uma estrutura de decisaõ para acessar o tipo de calcula desejado (switch case)
 //Criar funções para resolver os calculos
 //Soma, subtração, multiplicação e divisão
+let resp = num1, num2, op, opNome
+num1 = 20
+num2 = 30
+op = 2
+
+console.log("Escolha a operação desejada")
+console.log("1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão")
+console.log("Insira os números para realizar a operação")
 
 function calculoSoma(num1, num2) {
     return (num1 + num2)
@@ -13,30 +21,25 @@ function calculoMultiplicacao(num1, num2) {
 }
 
 function calculoDivisao(num1, num2) {
-    if(num1 === num2){
-    }else{
-        return(num1 / num2)
+    if (num2 === 0) {
+        return ("Impossivel dividir por 0")
+    } else {
+        return (num1 / num2)
     }
 }
-calculoDivisao()
- let resp = num1, num2, op, opNome
-    num1 = 20
-    num2 = 30
-    op = 1
 
 switch (op) {
-    case 1: resp = calculoSoma(num1, num2) 
-        opNome = 'soma'
+    case 1: resp = calculoSoma(num1, num2)
+        opNome = "soma"
         break
-
-    case 2: resp = calculoSubtracao(num1, num2) 
-        opNome = 'sutração'
+    case 2: resp = calculoSubtracao(num1, num2)
+        opNome = "sutração"
         break
-    case 3: resp = calculoMultiplicacao(num1, num2) 
-        opNome = 'multiplicação'
+    case 3: resp = calculoMultiplicacao(num1, num2)
+        opNome = "multiplicação"
         break
-    case 4: resp = calculoDivisao(num1, num2) 
-        opNome = 'divisao'
+    case 4: resp = calculoDivisao(num1, num2)
+        opNome = "divisao"
         break
 
 }
