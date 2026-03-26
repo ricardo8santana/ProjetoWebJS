d814a1232e4d2ab84b5a5e6435561e07712c88c5
ricardo8santana <luisriccardo@proton.me>
2026-03-26 14:23:29 -0300

    JavaScript Basic

---
+++ js/arrays.js
@@ -0,0 +1,15 @@
+// let numero = 10
+// const valor = 90
+
+// console.log(typeof(numero))
+// console.log(typeof(valor))
+
+//Array de linha -> vetor
+const nomes = ['Maria', 'José', 'Jesus']
+nomes[3] = 'Deus'
+console.log(nomes)
+console.log(nomes[2])
+console.log(nomes[3])
+console.log(nomes[0])
+console.log(nomes[2], nomes[0], nomes[1])
+console.log(`O filho ${nomes[2]} de ${nomes[0]} e ${nomes[1]}`)
