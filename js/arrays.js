d8943cded3c24121aad2fe5a9389ebd30d4ce5ad
ricardo8santana <luisriccardo@proton.me>
2026-03-26 15:30:01 -0300

    JavaScript Basic

--- js/arrays.js
+++ js/arrays.js
@@ -5,11 +5,31 @@
 // console.log(typeof(valor))
 
 //Array de linha -> vetor
-const nomes = ['Maria', 'José', 'Jesus']
-nomes[3] = 'Deus'
-console.log(nomes)
-console.log(nomes[2])
-console.log(nomes[3])
-console.log(nomes[0])
-console.log(nomes[2], nomes[0], nomes[1])
-console.log(`O filho ${nomes[2]} de ${nomes[0]} e ${nomes[1]}`)
+// const nomes = ['Maria', 'José', 'Jesus']
+// nomes[3] = 'Deus'
+// console.log(nomes)
+// console.log(nomes[2])
+// console.log(nomes[3])
+// console.log(nomes[0])
+// console.log(nomes[2], nomes[0], nomes[1])
+// console.log(`O filho ${nomes[2]} de ${nomes[0]} e ${nomes[1]}`)
+
+// const carros = []
+
+// carros[0] = "BMW"
+// carros[10] = "Fusca"
+// carros[15] = "Fiat"
+// carros[2] = "Monza"
+// carros[30] = "BYD"
+// carros[31] = "Voyage"
+// carros[1] = "Corsa"
+
+// console.log(carros)
+// console.log(carros[1])
+
+// Instanciando arrays
+const carros = new Array('Fusca', 1972, 'Preto', 'WHC 2538', 10500)
+console.log(carros)
+carros[5] = 'placa preta'
+console.log(`Valor do carro: R$${carros [4]}`)
+console.log(carros)
