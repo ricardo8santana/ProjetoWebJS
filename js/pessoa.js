037d5c7db1ba89d6d4bf230d72769acb08bbfad2
ricardo8santana <luisriccardo@proton.me>
2026-04-02 14:33:43 -0300

    JavaScript Basic

---
+++ js/pessoa.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: "João Andrade",
+    idade: 45,
+    cidade: "São Paulo",
+    atividades: ['Pescar', 'Viajar', 'Ler,', 'Escrever'],
+
+    cargo:['Administrador', 'Secetária', 'Gerente'],
+    estado:"São Paulo"
+}
+
+console.log(`Seu nome ${pessoa.nome} 
+    sua idade ${pessoa.idade} 
+    sua cidade ${pessoa.cidade} 
+    seu estado ${pessoa.estado} 
+    sua atividade ${atividades[2]}
+    seu cargo ${pessoa.cargo[3]}`)
