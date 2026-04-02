58710820be8bf93b8512259298cb78871ad689b3
ricardo8santana <luisriccardo@proton.me>
2026-04-02 16:09:55 -0300

    função map

--- js/map_exercicio.js
+++ js/map_exercicio.js
@@ -1,5 +1,12 @@
 
-const  sobrenome = ['Santana ', 'Jesus ']
+const  nomes = ['Ricardo ', 'Jesus ', 'Melissa', 'Janaína ', 'Nathali ']
 
-const nomeSobrenome = sobrenome.map((x) => x + 'de Santana')
-console.log(`Seu nome completo: ${nomeSobrenome}`)
+const sobrenome = ['da Silva', 'Antunes', 'Fagundes', 'Medeiros', 'Rufino']
+
+const novoNome = nomes.map((x) => x + 'de Santana')
+console.log(`Seu nome completo: ${novoNome}`)
+
+function sobreNome(x){
+    return x + 'de Santana'
+}
+console.log(nomes, sobrenome)
