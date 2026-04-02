dcc8093a7218f006091ab7fadac2d20f96fe5845
ricardo8santana <luisriccardo@proton.me>
2026-04-02 16:50:11 -0300

    função filter

---
+++ js/funcao_filter.js
@@ -0,0 +1,4 @@
+const words = ['spray', 'elite', 'exuberante', 'destrutivo', 'presente']
+
+const resultado = words.filter((words) => words.length > 6)
+console.log(`Palavras maiores que 6 caracteres: ${resultado}`)
