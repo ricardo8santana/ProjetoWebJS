485ccd7d5dc3da5317e55065986e9378ca2991fa
ricardo8santana <luisriccardo@proton.me>
2026-04-09 14:59:03 -0300

    Mudando elementos pelo JS

---
+++ js/elementos_id.js
@@ -0,0 +1,11 @@
+const escrevendoID = document.getElementById('demo')
+const titleID = document.getElementById('title')
+console.log(escrevendoID)
+console.log(titleID)
+
+//modificando o conteúdo do elemento selecionado pelo ID
+
+escrevendoID.innerHTML = "Por outro lado, o desafiador cenário globalizado oferece uma interessante oportunidade para verificação dos paradigmas corporativos."
+titleID.innerHTML = "Gostaria de enfatizar que a determinação clara de objetivos desafia a capacidade de equalização dos níveis de motivação departamental."
+console.log(escrevendoID)
+console.log(titleID)
---
+++ js/script.js
@@ -0,0 +1,8 @@
+const paragrafoID = document.getElementById('paragrafo')
+const paramID = document.getElementById('param')
+
+paragrafoID.innerHTML = "Por outro lado, a contínua expansão de nossa atividade faz parte de um processo de gerenciamento das condições inegavelmente apropriadas."
+paramID.innerHTML = "Percebemos, cada vez mais, que o desafiador cenário globalizado nos obriga à análise dos níveis de motivação departamental. "
+
+console.log(paragrafoID)
+console.log(paramID)
