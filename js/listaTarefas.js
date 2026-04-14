12708735be4037b328de71ac4ecf2c4172ae42ce
ricardo8santana <luisriccardo@proton.me>
2026-04-14 14:23:55 -0300

    Acessando os elementos html por id

---
+++ js/listaTarefas.js
@@ -0,0 +1,6 @@
+//Acessando os elementos html por id
+const inputTarefas = document.getElementById('inputTarefas')
+const btnAdicionar = document.getElementById('btnAdicionar')
+const listaTarefas = document.getElementById('istaTarefas')
+
+console.log(inputTarefas, btnAdicionar, listaTarefas)
