250ca87af2e3209ec50f4035b73a86784db021aa
ricardo8santana <luisriccardo@proton.me>
2026-04-14 15:54:24 -0300

    Adicionando função js

--- js/listaTarefas.js
+++ js/listaTarefas.js
@@ -2,8 +2,21 @@
 const inputTarefas = document.getElementById("inputTarefas")
 const btnAdicionar = document.getElementById("btnAdicionar")
 const listaTarefas = document.getElementById("listaTarefas")
+const btnLimpar = document.getElementById("btnLimpar")
 
 //Função no botão adicionar
-btnAdicionar.addEventListener("click", function(){
-    alert("Clique aqui...")
-})
+btnAdicionar.addEventListener("click", adicionarTarefas)
+
+btnLimpar.addEventListener("click", () => alert("Limpar ..."))
+
+function adicionarTarefas() {
+    const vTarefas = inputTarefas.value
+
+    if (vTarefas === "") {
+        alert("Digite uma tarefa")
+        return
+    }
+
+    const li = document.createElement("li")
+    li.innerText = vTarefas
+ }
