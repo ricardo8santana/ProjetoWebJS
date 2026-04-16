d96d384e9bc6c9f72288228fe8f622d982936592
ricardo8santana <luisriccardo@proton.me>
2026-04-16 15:10:06 -0300

    Validando campos formulário com js

---
+++ js/form_contato.js
@@ -0,0 +1,15 @@
+document.getElementById('form-contato').addEventListener('submit', function (event){
+    event.preventDefault()// impede o envio real para teste
+
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    const mensagem = document.getElementById('mensagem').value
+
+    if (nome  && email, mensagem){
+        alert(`Obrigado, ${nome}! Mensagem enviada com sucesso`
+            // toda lógica do sistema 
+        )
+    }else{
+        alert(`Por favor, preencha todos os campos`)
+    }
+})
